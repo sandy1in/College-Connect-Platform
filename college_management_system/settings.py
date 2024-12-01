547904696d3106403a806a2058b984a83a1d566b
sandeepmedikonda <sandeepmedikonda110@gmail.com>
2024-12-01 19:28:23 +0530

    for deployment2

--- college_management_system/settings.py
+++ college_management_system/settings.py
@@ -88,20 +88,27 @@ WSGI_APPLICATION = 'college_management_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+#     # 'default': {
+#     #     'ENGINE': 'django.db.backends.mysql',
+#     #     'NAME': 'django',
+#     #     'USER': os.environ.get('DB_USER'),
+#     #     'PASSWORD': os.environ.get('DB_PASS'),
+#     #     'HOST': '127.0.0.1',
+#     #     'PORT': '3307'
+#     # }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'django',
-    #     'USER': os.environ.get('DB_USER'),
-    #     'PASSWORD': os.environ.get('DB_PASS'),
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '3307'
-    # }
 }
+
 
 
 # Password validation
